a0a65854653b015ed15e358d3714788a5330c8bd
jake hopkins <hopin14@byu.edu>
2026-02-09 21:18:39 -0700

    react inclusion

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+export default function App() {
+  return <div className="body bg-dark text-light">
+    <footer className="site-footer">
+  <p><strong>Sociallearning</strong></p>
+  <p>CS 260 – Web Programming</p>
+  <p>Jake Hopkins</p>
+  <p><a href="https://github.com/jake-hoppy/Startup-BBY-/blob/main/README.md" target="_blank" rel="noopener noreferrer">GitHub</a></p>
+</footer></div>;
+}
